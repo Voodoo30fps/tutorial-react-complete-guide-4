2141f2a6dd2cf155b94b460a28938eed7833d9e4
Voodoo30fps <voodoo30fps@gmail.com>
2022-02-07 09:35:59 +0200

    validation update for login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -28,10 +28,20 @@ const Login = (props) => {
 
     const emailChangeHandler = (event) => {
         setEnteredEmail(event.target.value);
+
+        setFormIsValid(
+            event.target.value.includes('@') &&
+                enteredPassword.trim().length > 6
+        );
     };
 
     const passwordChangeHandler = (event) => {
         setEnteredPassword(event.target.value);
+
+        setFormIsValid(
+            event.target.value.includes('@') &&
+                enteredPassword.trim().length > 6
+        );
     };
 
     const validateEmailHandler = () => {
